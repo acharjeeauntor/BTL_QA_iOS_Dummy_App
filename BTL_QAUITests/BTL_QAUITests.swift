c55892a5ed3584f279e4825b76de9842682423b2
Auntor Acharja <acharjeeauntor@gmail.com>
2022-11-27 16:38:24 +0600

    Xcuitest basic practice (app launch, terminate, swipe, record and play)

--- BTL_QAUITests/BTL_QAUITests.swift
+++ BTL_QAUITests/BTL_QAUITests.swift
@@ -9,33 +9,59 @@ import XCTest
 
 class BTL_QAUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    override func setUpWithError() throws {
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//
+//        // In UI tests it is usually best to stop immediately when a failure occurs.
+//        continueAfterFailure = false
+//
+//        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+//    }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+//
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
+    
+    func testAppLaunchAndTerminatedWithSleepTime() throws{
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Sleep time in Second
+        sleep(10)
+        print("Test function execution Done")
+        app.terminate()
+    }
+    
+    func testRecordAndPlay() throws{
+        let app = XCUIApplication()
+        app.launch()
+        let element = XCUIApplication().windows.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+        element.tap()
+        element.children(matching: .staticText).matching(identifier: "Sr. QA I").element(boundBy: 1).tap()
+        debugPrint("The Record and Play test function run DONE")
+        
+    }
+    
+    func testSwipeDifferentSide() throws{
+        let app = XCUIApplication()
+        app.launch()
+        app.swipeLeft()
+        app.swipeRight()
+        app.swipeUp()
+        app.swipeDown()
+        debugPrint("Swipe Different Side test function run DONE")
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+    
+    
 }
